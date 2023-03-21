937c3ada0cd5cbd834ca0369234dfd0f71681c4b
Serrin Doscher <serrin.doscher53@myhunter.cuny.edu>
2023-03-20 20:56:19 -0400

    added header text and animation

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="header">
+        <h1 className="main-title">{"< Serrin Doscher />"}</h1>
+        <p className="subtitle">Portfolio Website</p>
+    </div>
+  )
+}
+
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
+import './stylesheets/headerstyle.css'
+
 
 const store = configureStore({
   reducer: {}
