1853ef135d8b0cc56c625d85b5d8455a79cf7f5c
serrin_sage <serrin.doscher53@myhunter.cuny.edu>
2023-03-23 22:25:23 -0400

    added right, left subtitle, gave each animation

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,8 +3,15 @@ const HomePage = () => {
     <div className="header">
         <h1 className="main-title">{"< Serrin Doscher />"}</h1>
         <div className="main-subtitles">
-            <p className="subtitle">Portfolio Website</p>
-            
+            <p className="left-subtitle">Software Engineer</p>
+            <p className="right-subtitle">Frontend Developer</p>
+        </div>
+        <div>
+            <p>Hello! Welcome to my website.
+                I am a Hunter College Graduate with a BS in Computer Science.
+                I also received a Technical Diploma in Software Engineering from the Flatiron School.
+                With a passion for Frontend Development I have a strong understanding of web development tools as well as several backend frameworks.
+            </p>
         </div>
     </div>
   )
